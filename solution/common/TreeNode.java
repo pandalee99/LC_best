ed6543b492be77a29e6a297beb6951c62430afd4
pandalee99 <1162953505@qq.com>
2024-05-17 00:55:45 +0800

    236

---
+++ solution/common/TreeNode.java
@@ -0,0 +1,8 @@
+package common;
+
+ public class TreeNode {
+      int val;
+      public TreeNode left;
+      public TreeNode right;
+      public TreeNode(int x) { val = x; }
+  }
---
+++ solution/t236.java
@@ -0,0 +1,15 @@
+import common.TreeNode;
+
+public class t236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==null) return null;
+        if(root==p || root == q){
+            return root;
+        }
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left!=null&&right!=null)return root;
+        if(left!=null)return left;
+        return right;
+    }
+}
